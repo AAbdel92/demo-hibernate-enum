fb22c1cb9db986f7d557755be6bdf97c173850f1
AAbdel92 <ahmar.abdel@gmail.com>
2017-04-10 10:59:52 +0200

    first commit

---
+++ src/main/java/fr/simplon/laposte/DemoHibernateEnumApplication.java
@@ -0,0 +1,12 @@
+package fr.simplon.laposte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHibernateEnumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHibernateEnumApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/simplon/laposte/DemoHibernateEnumApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.simplon.laposte;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoHibernateEnumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
